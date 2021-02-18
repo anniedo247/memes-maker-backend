5fdcbe33fea72f44f54f993c1372d49516d62e32
Do Thi Hai Van <annie@NGUYENs-MacBook-Pro.local>
2021-02-18 07:52:24 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+// All route of Meme
+const memeAPI = require("./meme.api");
+router.use("/memes",memeAPI);
+
+module.exports = router;
